7be51139ffaf79e70a8c6ca07659750559d71761
Karthick Selvaraj <karthick@mallow-tech.com>
2020-06-11 23:25:14 +0530

    Simplified code.

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -10,107 +10,94 @@ import SwiftUI
 
 struct ContentView: View {
     
-   var cornerRadius: CGFloat = 20
-   
-   var body: some View {
-       VStack(spacing: 40) {
-           
-           // =========== Heading ===========
-           Text("Neumorphism!!")
-           .font(Font.system(.title, design: .rounded))
-           .bold()
-           .foregroundColor(.pink)
-           .padding()
-           
-           HStack(spacing: 40) {
-               // =========== 1 ===========
-               ZStack {
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fillAndFrame()
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fillAndFrame()
-                       .darkShadow()
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fillAndFrame()
-                       .lightShadow()
-                   Image(systemName: "1.circle.fill")
-                       .asOverlay()
-               }
-               
-               // =========== 2 ===========
-               ZStack {
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fillAndFrame()
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fillAndFrame()
-                       .darkShadow()
-                       .blur(radius: 5)
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fillAndFrame()
-                       .lightShadow()
-                       .blur(radius: 5)
-                   Image(systemName: "2.circle.fill")
-                       .asOverlay()
-               }
-               
-           }
-           
-           HStack(spacing: 40) {
-               // =========== 3 ===========
-               ZStack {
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fillAndFrame()
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fillAndFrame()
-                       .blur(radius: 15)
-                       .opacity(0.7)
-                       .darkShadow()
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fillAndFrame()
-                       .blur(radius: 15)
-                       .opacity(0.7)
-                       .lightShadow()
-                   Image(systemName: "3.circle.fill")
-                       .asOverlay()
-               }
-               .mask(
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fill(Color.red)
-                       .frame(width: 150, height: 150)
-               )
-               
-               // =========== 4 ===========
-               ZStack {
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fillAndFrame()
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fill(Color("kMainBg"))
-                       .frame(width: 130, height: 130)
-                       .blur(radius: 15)
-                       .opacity(0.7)
-                       .darkShadow()
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fill(Color("kMainBg"))
-                       .frame(width: 130, height: 130)
-                       .blur(radius: 15)
-                       .opacity(0.7)
-                       .lightShadow()
-                   Image(systemName: "4.circle.fill")
-                       .asOverlay()
-               }
-               .mask(
-                   RoundedRectangle(cornerRadius: cornerRadius)
-                       .fill(Color.red)
-                       .frame(width: 170, height: 170)
-               )
-               
-           }
-           
-       }
-       .frame(maxWidth: .infinity, maxHeight: .infinity)
-       .background(Color("kMainBg"))
-       .edgesIgnoringSafeArea(.all)
-   }
+    var cornerRadius: CGFloat = 20
+    
+    var body: some View {
+        VStack(spacing: 40) {
+            
+            // =========== Heading ===========
+            Text("Neumorphism!!")
+                .font(Font.system(.title, design: .rounded))
+                .bold()
+                .foregroundColor(.pink)
+                .padding()
+            
+            HStack(spacing: 40) {
+                // =========== 1 ===========
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .fillAndFrame()
+                    .darkShadow()
+                    .lightShadow()
+                    .overlay(
+                        Image(systemName: "1.circle.fill")
+                            .asOverlay()
+                    )
+                
+                // =========== 2 ===========
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .fillAndFrame()
+                    .darkShadow()
+                    .lightShadow()
+                    .blur(radius: 5)
+                    .overlay(
+                        Image(systemName: "2.circle.fill")
+                            .asOverlay()
+                    )
+            }
+            
+            HStack(spacing: 40) {
+                // =========== 3 ===========
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .fillAndFrame()
+                    .blur(radius: 15)
+                    .opacity(0.7)
+                    .darkShadow()
+                    .lightShadow()
+                    .overlay(
+                        Image(systemName: "3.circle.fill")
+                            .asOverlay()
+                    )
+                    .mask(
+                        RoundedRectangle(cornerRadius: cornerRadius)
+                            .fill(Color.red)
+                            .frame(width: 150, height: 150)
+                    )
+                
+                
+                // =========== 4 ===========
+                ZStack {
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .fillAndFrame()
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .fill(Color("kMainBg"))
+                        .frame(width: 135, height: 135)
+                        .blur(radius: 15)
+                        .opacity(0.7)
+                        .darkShadow()
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .fill(Color("kMainBg"))
+                        .frame(width: 135, height: 135)
+                        .blur(radius: 15)
+                        .opacity(0.7)
+                        .lightShadow()
+                    .overlay(
+                        Image(systemName: "4.circle.fill")
+                        .asOverlay()
+                    )
+                }
+                .mask(
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .fill(Color.red)
+                        .frame(width: 170, height: 170)
+                )
+                
+            }
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("kMainBg"))
+        .edgesIgnoringSafeArea(.all)
+    }
     
 }
 
@@ -147,11 +134,11 @@ extension View {
 extension Image {
     
     func asOverlay() -> some View {
-          self
-          .resizable()
-          .frame(width: 50, height: 50)
-          .aspectRatio(contentMode: .fit)
-          .foregroundColor(.pink)
+        self
+            .resizable()
+            .frame(width: 50, height: 50)
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.pink)
     }
     
 }
